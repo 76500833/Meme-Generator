d75c8e0d26be05b3744268ea7c488cdf8fe5a0f4
Ethan Metz <76500833@protonmail.com>
2024-09-05 16:17:14 -0800

    images apearing

--- meme-generator/src/components/Form.jsx
+++ meme-generator/src/components/Form.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-
+import memesData from "../memesData.js"
 function Form() {
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        //Gets random number
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
+
+
     return (
-        <>  
-        <main>
-            <div id="form-container" action="">
-                <input type="text" 
-                id="form-input"
-                placeholder="Top Text"
-                />
-                <input type="text" 
-                id="form-input"
-                placeholder="Bottom Text"
-                />
-            <button id="form-button">Get A New Meme Image</button>
-            </div>
-        </main>
-        
+        <>
+            <main>
+                <div id="form-container" action="">
+                    <input type="text"
+                        id="form-input"
+                        placeholder="Top Text"
+                    />
+                    <input type="text"
+                        id="form-input"
+                        placeholder="Bottom Text"
+                    />
+                    <button
+                        onClick={getMemeImage}
+                        id="form-button">Get A New Meme Image</button>
+                </div>
+                <div id="meme-container">
+                    <img src={memeImage} id="meme-image" alt="" />
+                </div>
+            </main>
         </>
     )
 }

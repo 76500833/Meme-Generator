636466f7a46f45e9703a8fab08e42c13a3aef16a
Ethan Metz <76500833@protonmail.com>
2024-09-12 19:48:48 -0800

    switched to state objects

--- meme-generator/src/components/Form.jsx
+++ meme-generator/src/components/Form.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import memesData from "../memesData.js"
 function Form() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
-        //Gets random number
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+
     }
-
-
 
     return (
         <>
@@ -36,4 +43,5 @@ function Form() {
         </>
     )
 }
+
 export default Form;
